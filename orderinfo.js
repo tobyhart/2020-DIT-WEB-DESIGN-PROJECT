5217ef9e7916606f1c7da7fe75b074135a6b3213
Toby Hart <TobyHart@Tobys-Macbook-Pro-3.local>
2020-06-09 22:53:31 +1200

    New JS page for Order Info

---
+++ orderinfo.js
@@ -0,0 +1,99 @@
+var orderid = localStorage.getItem("orderid");
+
+if (orderid == 1) {
+    document.getElementById('order-name').innerHTML = 'AVA Robot';
+} 
+if (orderid == 2) {
+    document.getElementById('order-name').innerHTML = 'AVA App Manuals';
+}
+if (orderid == 3) {
+    document.getElementById('order-name').innerHTML = 'Grand Fayonette';
+}
+if (orderid == 4) {
+    document.getElementById('order-name').innerHTML = 'Smart Home Package';
+}
+
+
+
+if (orderid == 5)  {
+    document.getElementById('order-name').innerHTML = 'Surface Laptop';
+} 
+if (orderid == 6) {
+    document.getElementById('order-name').innerHTML = 'Surface Pro';
+}
+if (orderid == 7) {
+    document.getElementById('order-name').innerHTML = 'Hillbook';
+}
+if (orderid == 8) {
+    document.getElementById('order-name').innerHTML = 'AVAbook';
+}
+
+
+
+if (orderid == 9)  {
+    document.getElementById('order-name').innerHTML = 'Mercury Desktop Computer';
+} 
+if (orderid == 10) {
+    document.getElementById('order-name').innerHTML = 'Mercury Pro Desktop Computer';
+}
+if (orderid == 11) {
+    document.getElementById('order-name').innerHTML = 'MercuryTop Desktop Computer';
+}
+if (orderid == 12) {
+    document.getElementById('order-name').innerHTML = 'MercuryTop Pro';
+}
+
+
+
+if (orderid == 13)  {
+    document.getElementById('order-name').innerHTML = 'Avalanche 7';
+} 
+if (orderid == 14) {
+    document.getElementById('order-name').innerHTML = 'Avalanche 7C';
+}
+if (orderid == 15) {
+    document.getElementById('order-name').innerHTML = 'Avalanche 7S';
+}
+if (orderid == 16) {
+    document.getElementById('order-name').innerHTML = 'Avalanche Pro';
+}
+
+
+
+
+if (orderid == 17)  {
+    document.getElementById('order-name').innerHTML = 'Tokyo 3';
+} 
+if (orderid == 18) {
+    document.getElementById('order-name').innerHTML = 'Tokyo 3G';
+}
+if (orderid == 19) {
+    document.getElementById('order-name').innerHTML = 'Tokyo 3S';
+}
+if (orderid == 20) {
+    document.getElementById('order-name').innerHTML = 'Tokyo Pro';
+}
+
+
+
+if (orderid == 21)  {
+    document.getElementById('order-name').innerHTML = 'Manhattan Tablet';
+} 
+if (orderid == 22) {
+    document.getElementById('order-name').innerHTML = 'Manhattan Pro Tablet';
+}
+if (orderid == 23) {
+    document.getElementById('order-name').innerHTML = 'Crete Security Phone';
+}
+if (orderid == 24) {
+    document.getElementById('order-name').innerHTML = 'Crete Pro Phone';
+}
+
+
+
+if (orderid == 25) {
+    document.getElementById('order-name').innerHTML = 'Grand Bottocelli';
+}
+if (orderid == 26) {
+    document.getElementById('order-name').innerHTML = 'HillCorp Astro';
+}
